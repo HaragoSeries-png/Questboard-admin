7434b86ccc152c1c91ca0c2cb6528e5cc1abdbb3
Siraw1998 <SirawNorth@gmail.com>
2021-04-26 22:15:54 +0700

    Faith dashboard

---
+++ src/components/categorybar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id='chart'>
+    </div>
+</template>
+<script>
+var options = {
+  chart: {
+    type: 'bar'
+  },
+  series: [{
+    name: 'amount',
+    data: [20,30,40,50,100]
+  }],
+  xaxis: {
+    categories: ['Handicraft','Advice','Education','Accident','Housework','Find friends','Food and home economics','Traffic']
+  }
+}
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+chart.render();
+</script>
+<style >
+#chart {
+  max-width: 650px;
+  margin: 35px auto;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,4 +1,19 @@
 <template>
-    
+<dailyquest>
+</dailyquest>
+
+
+
 </template>
+<script>
+import Dailyquest from "..//components/Dailyquest"
+export default {
+    name : "Dashboard",
+  components : {Dailyquest},
+}
+
+</script>
+<style>
+
+</style>>
 
