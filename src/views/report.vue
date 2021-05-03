08edaa600260a5fe6497062f0e6b21602b3b6bfc
Siraw1998 <SirawNorth@gmail.com>
2021-05-03 20:31:12 +0700

    Report complete

--- src/views/report.vue
+++ src/views/report.vue
@@ -3,23 +3,22 @@
       <h1>
           Report page
       </h1>
-      
-    <v-card v-for="report in reports" :key="report._id" class="card"
-      color="#26c6da"
-      height="300"
-      style="width:300;height:300"
-    > 
-      <v-card-text  class="sender">
-       Sender: {{report.sender}}
-      </v-card-text>
+  <v-row style="padding-left:5%;">
+  <v-card style="width:300px;margin-top:5%;margin-left:5%;" v-for="report in reports" :key="report._id" >
+    <v-card-text>
+          Send by   {{report.sender}}
+    </v-card-text>
       <v-card-actions>
-        <v-list-item>
-          <v-list-item-content  class="card-content">
-            <v-card-textarea class="card-text">Detail: {{report.message}} </v-card-textarea>
+        <v-list-item  >
+          <v-list-item-content style="max-height:200px;overflow-y:auto;" >
+            <v-card-textarea >Detail: {{report.message}} </v-card-textarea>
           </v-list-item-content>
         </v-list-item>
       </v-card-actions>
-    </v-card>
+  </v-card>
+  
+  </v-row>
+  
   </div>
 </template>
 
@@ -46,35 +45,4 @@ export default {
 </script>
 
 <style>
-.card{
-    margin-top:5%;
-    width: 600px;
-    height:250px;
-    border: 1px solid black;
-}
-.card-content{
-    line-height: 1.5;
-    font-size: 0.9em;
-    padding: 15px;
-    border-top: 2px solid #333333;
-    border-right: 2px solid #333333;
-    border-left: 2px solid #333333;
-    border-bottom: 2px solid #333333;
-}
-.card-text{
-  overflow: auto;
-}
-.sender{
-  padding: 15px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    color: #555555;
-    background: #eeeeee;
-    font-size: 1.0em;
-    border-top: 2px solid #333333;
-    border-right: 2px solid #333333;
-    border-left: 2px solid #333333;
-    border-bottom: 2px solid #333333;
-}
 </style>
